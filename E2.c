e9adb68925cedd479d0f32fc9c0c89b0b7c1fcaf
Alifrantus <tkduy04@gmail.com>
2026-03-30 23:04:08 +0700

    Update E2.c

--- E2.c
+++ E2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_BUCKETS 1000
 #define MULTIPLIER 31
-#define MAXLEN 100
+#define MAX_LEN 100
 
 struct wordrec
 {
@@ -15,39 +15,118 @@ struct wordrec
 
 struct wordrec* walloc(const char* str)
 {
-    struct wordrec* p = (struct wordrec*)malloc(sizeof(struct wordrec));
-    if(p != NULL)
+    struct wordrec* p=(struct wordrec*)malloc(sizeof(struct wordrec));
+    if(p!=NULL)
     {
-        p -> count = 0;
-        p -> word = strdup(str);
-        p -> next = NULL;
+        p->count=0;
+        p->word=strdup(str); /* creates a duplicate */
+        p->next=NULL;
     }
     return p;
 }
 
-struct wordrec* table[MAXLEN];
+/* hash bucket */
+struct wordrec* table[MAX_LEN];
 
+/*
+@function hashstring
+@desc produces hash code for a string
+multipliers 31,35 have been found to work well
+*/
 unsigned long hashstring(const char* str)
 {
-    unsigned long hash = 0;
-    while (*str) 
+    unsigned long hash=0;
+    while(*str)
     {
-        hash = hash * MULTIPLIER + *str;
-        str ++;
+        hash=hash*MULTIPLIER+*str;
+        str++;
     }
-    return  hash%MAX_BUCKETS;
+    return hash%MAX_BUCKETS;
 }
 
+/*
+@function lookup
+@desc returns a pointer to the word or creates
+it if required
+*/
 struct wordrec* lookup(const char* str, int create)
 {
-    unsigned long hash = hashstring(str);
-    struct wordrec* wp = table[hash];
-    struct wordrec* curr = NULL;
-    for(curr = wp; curr != NULL; curr = curr -> next)
-        if(strcmp(curr -> word, str) == 0) return curr;
+    unsigned long hash=hashstring(str);
+    struct wordrec* wp=table[hash];
+    struct wordrec* curr=NULL;
+    for(curr=wp; curr!=NULL; curr=curr->next)
+        if(strcmp(curr->word, str)==0) /* found */
+        {
+            return curr;
+        }
+
+    /* not found */
     if(create)
     {
-        curr = (struct wordrec*)malloc(sizeof(struct wordrec));
-        curr -> word = strdup(str);
+        curr=(struct wordrec*)malloc(sizeof(struct wordrec));
+        curr->word=strdup(str);
+        curr->count=0;
+        /* add to front */
+        curr->next=table[hash];
+        table[hash]=curr;
     }
+    return curr;
+}
+
+/*
+@function cleartable()
+@desc reclaims memory
+*/
+void cleartable()
+{
+    struct wordrec* wp=NULL, *p=NULL;
+    int i=0;
+    for(i=0; i<MAX_BUCKETS; i++)
+    {
+        wp=table[i];
+        while(wp)
+        {
+            p=wp;
+            wp=wp->next;
+            free(p->word);
+            free(p);
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    FILE* fp=fopen("book.txt", "r");
+    char word[1024]; /* big enough */
+    struct wordrec* wp=NULL;
+    int i=0;
+
+    memset(table, 0, sizeof(table));
+
+    /* read from input */
+    while(1)
+    {
+        if(fscanf(fp, "%s", word)!=1)
+            break;
+        wp=lookup(word, 1); /* create if doesn't exist */
+        wp->count++;
+    }
+    fclose(fp);
+
+    /*
+    print all words with frequency >100
+    */
+    for(i=0; i<MAX_BUCKETS; i++)
+    {
+        for(wp=table[i]; wp!=NULL; wp=wp->next)
+        {
+            if(wp->count>1000)
+            {
+                printf("%s-->%ld\n", wp->word, wp->count);
+            }
+        }
+    }
+
+    cleartable();
+    return 0;
 }
